42927aa89938ec7d798fd27bd24ebe5acae8fc05
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2025-11-03 17:19:11 -0500

    Update Oh-my-zsh installation and configuration
    Refactor installation process for Oh-my-zsh and related configurations.

--- config.sh
+++ config.sh
@@ -40,15 +40,6 @@ else
   echo "Xcode-select Found"
 fi
 
-# Install Oh-my-zsh
-if [[ $(whereis zsh) == '' ]]
-then
-  echo "zsh not found ... installing Oh-my-zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-  echo "Zsh Found"
-fi
-
 # Install Neovim
 if [[ $(nvim --version) == '' ]]
 then
@@ -59,19 +50,20 @@ else
 fi
 
 # Install Autocompletions
-if [[ $(brew list ripgrep) == '' ]]
-then
-  echo "Ripgrep not found ... installing Ripgrep"
-  brew install ripgrep
-else
-  echo "Ripgrep Found"
-fi
+# if [[ $(brew list ripgrep) == '' ]]
+# then
+#  echo "Ripgrep not found ... installing Ripgrep"
+#  brew install ripgrep
+#else
+#  echo "Ripgrep Found"
+#fi
 
 # Install Node Version Manager (NVM)
-if [[ $(nvm --version) == '' ]]
+if [[ ${NVM_DIR} == "" ]]
 then
   echo "NVM not found ... installing NVM"
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  echo $(nvm --version)
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 else
   echo "NVM Found"
 fi
@@ -84,24 +76,59 @@ then
 else
   echo "PyEnv Found"
 fi
+  
+# Install Oh-my-zsh
+if [[ ${ZSH} != "" ]]
+then
+  echo "--------------------------------------------"
+  echo "|                  WARNING                  |"
+  echo "--------------------------------------------"
+  echo "Current instalation of Oh-My-Zsh was found, if you continue, everything inside ${ZSH} will be erased"
+  echo "Do you want to proceed? [y/n]:"
+  read remove_zsh_response
+else
+  echo "Oh-My-Zsh not found"
+fi
 
 
-# Install PowerLevel10K Theme
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+if [[ ${remove_zsh_response} == [yY] ]]
+then
+  echo ">>> Removing ${ZSH}"
+  rm -rf ${ZSH}
+else
+  echo ">>> ${ZSH}/.oh-my-zsh won't be removed, exiting program"
+  exit 1
+fi
 
-# Install color theme
-curl https://raw.githubusercontent.com/josean-dev/dev-environment-files/main/coolnight.itermcolors --output ~/Downloads/coolnight.itermcolors
-
-# Install ZSH plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlightingo
-
+# Install Oh-my-zsh
+echo ">>> Installing Oh-My-Zsh"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Copy zsh configuration
 cp ./.zshrc ../
 
+# Install PowerLevel10K Theme
+echo ">>> Installing Powerline10K Theme"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# Install color theme
+downloads_folder="~/Downloads/coolnight.itermcolors" # I couldn't figure out how to extract the folder into a variable, TODO
+echo ">>> Installing color theme to [~/Downloads/coolnight.itermcolors]"
+curl https://raw.githubusercontent.com/josean-dev/dev-environment-files/main/coolnight.itermcolors --output ~/Downloads/coolnight.itermcolors
+
+# Install ZSH plugins
+echo ">>> Installing ZSH pluggins [zsh-autosuggestions, zsh-syntax-highlighting]"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # Copy powerline configuration
-cp ./.p10k.zsh ../
+echo ">>> Moving Powerline10K configuration ..."
+cp ./.p10k.zsh ../  
+
 
 # Copy NeoVim configuration
+echo ">>> Copying Neovim configuration ..."
 cp -p -r ./nvim ../.config/
+
+# Source new configuration
+source ../.zshrc
