e369ef8e622a6d6f546ffa7b72108209b24b470c
Juan Diego Campos Neira <juan1diego2campos3neira4@gmail.com>
2026-06-06 13:07:48 -0500

    Creating a symlink for powerline config file .p10k.zsh

--- config.sh
+++ config.sh
@@ -122,9 +122,9 @@ echo ">>> Installing ZSH pluggins [zsh-autosuggestions, zsh-syntax-highlighting]
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Copy powerline configuration
-echo ">>> Moving Powerline10K configuration ..."
-cp ./.p10k.zsh ../  
+# Create symbolic link for powerline config
+echo ">>> Creating symlink for Powerline10K configuration ..."
+ln -sf ~/MacTerminal/.p10k.zsh ~/
 
 
 # Copy NeoVim configuration
